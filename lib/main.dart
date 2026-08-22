8e6e297b815c8dbb088890b2348851ec20d3ddf6
hassenmlaikia <hassenmlaikia69@gmail.com>
2026-08-22 01:06:28 +0100

    chore: show new SplashScreen as app entry (replace WassalniApp in main.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wassalni/app/app.dart';
+import 'package:wassalni/core/theme/wassalni_theme.dart';
+import 'package:wassalni/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Firebase initialization would go here
-  // await Firebase.initializeApp();
-
   runApp(
     const ProviderScope(
-      child: WassalniApp(),
+      child: _WassalniRunner(),
     ),
   );
+}
+
+class _WassalniRunner extends StatelessWidget {
+  const _WassalniRunner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WASSALNI',
+      debugShowCheckedModeBanner: false,
+      theme: WassalniTheme.darkTheme,
+      home: const SplashScreen(),
+    );
+  }
 }
